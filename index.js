21efb7c4ce6a179306af0afa99e285fbe5d95621
Anu-tochka <anuta-zh@mail.ru>
2021-09-29 18:05:42 +0300

    start

---
+++ index.js
@@ -0,0 +1,7 @@
+// console.log(process.argv);
+// console.log('Hello NodeJS', process.argv[2], process.argv[3]);
+// const colors = require('colors');
+//
+// console.log(colors.green('Hello'), colors.yellow('Node JS'))
+
+
